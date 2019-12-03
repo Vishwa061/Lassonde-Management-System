fd861857ea5a4ef8f1d6a9b96b460becd55e2881
Vishwa Perera <pereravishwa061@gmail.com>
2019-12-02 21:09:32 -0500

    Added getAll functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+// const sanitizer = require("./sanitizer");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
@@ -8,19 +9,19 @@ var db = admin.firestore();
 
 
 exports.getStudents = functions.https.onRequest((request, response) => {
-    // var dataOfKey = request[0].text;
-    // var dataOfValue = request[1].text;
+    var key = "id";        //request[0].text;
+    var value = "157151182";      //request[1].text;
 
 
-    // Checks if request data is valid
-    // if (!(typeof dataOfKey === "string" && typeof dataOfValue === "string") || dataOfKey.length === 0 || dataOfValue.length === 0) {
+    // // Checks if request data is valid
+    // if (!(typeof key === "string" && typeof value === "string") || key.length === 0 || value.length === 0) {
     //     throw new functions.https.HttpsError("invalid-argument", "The function must be called with " +
     //         "two arguments 'key' and 'value' which must both be Strings and non-empty.");
     // }
 
     // Queries Cloud Firestore for requested student(s)
     var students = [];
-    var unsub = db.collection("students").where("id", "==", "123456789")
+    var unsub = db.collection("students").where(key, "==", value)
         .onSnapshot(querySnapshot => {
             querySnapshot.forEach(doc => {
                 students.push(doc.data());
@@ -34,11 +35,60 @@ exports.getStudents = functions.https.onRequest((request, response) => {
         unsub();
     }, 3000);
 
-    
+
     return;
 });
 
+exports.getAllStudents = functions.https.onRequest((request, response) => {
+    var students = [];
+    var test = db.collection("students").get()
+    .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            students.push(doc.data());
+        });
+        console.log("Students Found in Database: ", students);
+        response.send(students);
+        return;
+    });
+    test.catch(error => {
+        console.log(error);
+    });
+    return;
+});
 
+exports.getAllProfessors = functions.https.onRequest((request, response) => {
+    var professors = [];
+    db.collection("students").get()
+    .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            professors.push(doc.data());
+        });
+        console.log("Professors Found in Database: ", professors);
+        response.send(professors);
+        return;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    return;
+});
+
+exports.getAllStaff = functions.https.onRequest((request, response) => {
+    var staff = [];
+    db.collection("students").get()
+    .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            staff.push(doc.data());
+        });
+        console.log("Students Found in Database: ", staff);
+        response.send(staff);
+        return;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    return;
+});
 
 
 // TO TEST:
