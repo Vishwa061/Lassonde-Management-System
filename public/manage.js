9b3fe1becbde43eaffd3cadd0a96949a38f5d3d9
Vishwa Perera <pereravishwa061@gmail.com>
2019-12-24 13:36:33 -0500

    all

--- public/manage.js
+++ public/manage.js
@@ -4,9 +4,9 @@ var star = [];
 // Creates the top row of table "Student Name" "Student Number" etc
 function createTable(name,id,info) {
 
-    if (create == true) {
+    if(create == true){
         var maintable = document.createElement("TABLE");
-        maintable.setAttribute("id", "table");
+        maintable.setAttribute("id","table");
         var row1 = document.createElement("TR");
         var th1 = document.createElement("TH");
         var th2 = document.createElement("TH");
@@ -15,10 +15,10 @@ function createTable(name,id,info) {
         var txt1 = document.createTextNode(name);
         var txt2 = document.createTextNode(id);
         var txt3 = document.createTextNode(info);
-        th1.setAttribute("width", "50%");
-        th2.setAttribute("width", "35%");
-        th3.setAttribute("width", "10%");
-        th4.setAttribute("width", "5%");
+        th1.setAttribute("width","50%");
+        th2.setAttribute("width","35%");
+        th3.setAttribute("width","10%");
+        th4.setAttribute("width","5%");
         th1.appendChild(txt1);
         th2.appendChild(txt2);
         th3.appendChild(txt3);
@@ -34,12 +34,7 @@ function createTable(name,id,info) {
     }
 }
 
-// Clears all but the first row in the table
-function clearTable(table) {
-    for (var i = table.rows.length - 1; i > 0; i--) {
-        table.deleteRow(i);
-    }
-}
+// Creates table rows and adds data to them
 
 function clearTable(table) {
     for (var i = table.rows.length - 1; i > 0; i--) {
@@ -59,7 +54,7 @@ function generateTable(data) {
         var cell3 = document.createElement("TD");
         cell1.innerHTML = data[i].name;
         cell2.innerHTML = data[i].id;
-        cell3.innerHTML = "update";
+        cell3.innerHTML = data[i].email;
         rows.appendChild(cell1);
         rows.appendChild(cell2);
         rows.appendChild(cell3);
@@ -87,7 +82,7 @@ function generateCourseTable(data) {
     }
 }
 
-function generateStaffTable(data,thirdcol) {
+function generateStaffTable(data) {
     var main = document.getElementById("table");
     clearTable(main);
     for (var i = 0; i < data.length; i++) {
@@ -97,8 +92,7 @@ function generateStaffTable(data,thirdcol) {
         var cell3 = document.createElement("TD");
         cell1.innerHTML = data[i].name;
         cell2.innerHTML = data[i].id;
-        // cell3.innerHTML = data[i].role;
-        cell3.innerHTML = "";
+        cell3.innerHTML = data[i].roles;
         rows.appendChild(cell1);
         rows.appendChild(cell2);
         rows.appendChild(cell3);
@@ -106,7 +100,7 @@ function generateStaffTable(data,thirdcol) {
         main.appendChild(rows);
     }
 }
-function generateExamTable(data,thirdcol) {
+function generateExamTable(data) {
     var main = document.getElementById("table");
     clearTable(main);
     for (var i = 0; i < data.length; i++) {
@@ -116,11 +110,11 @@ function generateExamTable(data,thirdcol) {
         var cell3 = document.createElement("TD");
         cell1.innerHTML = data[i].CAT;
         cell2.innerHTML = data[i].date;
-        // cell3.innerHTML = data[i].location;
-        cell3.innerHTML = "";
+        cell3.innerHTML = data[i].location;
         rows.appendChild(cell1);
         rows.appendChild(cell2);
         rows.appendChild(cell3);
+        var main = document.getElementById("table");
         main.appendChild(rows);
     }
 }
@@ -138,7 +132,7 @@ function showSpecificExamData() {
     request.onload = function() {
         // Begin accessing JSON data here
         var data = JSON.parse(this.response);
-        console.log(data[0]);
+        console.log(data[0].name);
         star = data;
         generateExamTable(data)
     }
@@ -202,36 +196,23 @@ function showSpecificStudentData() {
     request.onload = function() {
         // Begin accessing JSON data here
         var data = JSON.parse(this.response);
+        console.log(data[0].name);
         star = data;
-        generateCourseTable(data);
-    }
-
-    request.send();
-}
-
-// Makes request to API and adds a new student to Firebase
-function addNewStudentData() {
-    var stuname = document.getElementById("stuname").value;
-    var stunum = document.getElementById("stunum").value;
-    var params = "?id=" + stunum + "&name=" + stuname + "&courses" + "&email";
-    var request = new XMLHttpRequest();
-    request.open('POST', 'https://us-central1-management-system-be9f9.cloudfunctions.net/addStudent' + params, true)
-    request.onload = function () {
-        // Begin accessing JSON data here
-        alert(this.response);
+        generateTable(data)
     }
 
     request.send()
 }
 
-function addNewCourseData() {
-    var coursename = document.getElementById("coursename").value;
-    var courseCAT = document.getElementById("courseCAT").value;
-    var coursesubject = document.getElementById("coursesubject").value;
-    var params = "?CAT=" + courseCAT + "&name=" + coursename + "&subject=" + coursesubject;
+// Makes request to API and adds a new student to Firebase
+function addNewStudentData() {
+
+    var stuname = document.getElementById("stuname").value;
+    var stunum = document.getElementById("stunum").value;
+    var params = "?id=" + stunum + "&name=" + stuname + "&courses" + "&email";
     var request = new XMLHttpRequest();
-    request.open('POST', 'https://us-central1-management-system-be9f9.cloudfunctions.net/addCourse' + params, true)
-    request.onload = function () {
+    request.open('POST', 'https://us-central1-management-system-be9f9.cloudfunctions.net/addStudent'+params, true)
+    request.onload = function() {
         // Begin accessing JSON data here
         alert(this.response);
     }
@@ -241,24 +222,12 @@ function addNewCourseData() {
 
 // Makes request to API and removes a Student
 function removeStudentData() {
+
     var stunum = document.getElementById("stuname2").value;
     var params = "?id=" + stunum;
     var request = new XMLHttpRequest();
-    request.open('DELETE', 'https://us-central1-management-system-be9f9.cloudfunctions.net/removeStudent' + params, true)
-    request.onload = function () {
-        // Begin accessing JSON data here
-        alert(this.response);
-    }
-
-    request.send()
-}
-
-function removeCourseData() {
-    var courseCATremoved = document.getElementById("courseCATremoved").value;
-    var params = "?CAT=" + courseCATremoved;
-    var request = new XMLHttpRequest();
-    request.open('DELETE', 'https://us-central1-management-system-be9f9.cloudfunctions.net/removeCourse' + params, true)
-    request.onload = function () {
+    request.open('DELETE', 'https://us-central1-management-system-be9f9.cloudfunctions.net/removeStudent'+params, true)
+    request.onload = function() {
         // Begin accessing JSON data here
         alert(this.response);
     }
@@ -271,25 +240,11 @@ function showAllStudentData() {
     createTable("Student Name","Student Number","Email Add.");
     var request = new XMLHttpRequest()
     request.open('GET', 'https://us-central1-management-system-be9f9.cloudfunctions.net/getAllStudents', true)
-    request.onload = function () {
+    request.onload = function() {
         // Begin accessing JSON data here
         var data = JSON.parse(this.response)
         star = data
-        generateTable(data);
-    }
-
-    request.send()
-}
-
-function showAllCourseData() {
-    createTable("Course Name", "Course CAT", "Subject");
-    var request = new XMLHttpRequest()
-    request.open('GET', 'https://us-central1-management-system-be9f9.cloudfunctions.net/getAllCourses', true)
-    request.onload = function () {
-        // Begin accessing JSON data here
-        var data = JSON.parse(this.response)
-        star = data
-        generateCourseTable(data)
+        generateTable(data)
     }
 
     request.send()
@@ -381,7 +336,7 @@ function addNewCourseData() {
     var coursename = document.getElementById("coursename").value;
     var courseCAT = document.getElementById("courseCAT").value;
     var coursesubject = document.getElementById("coursesubject").value;
-    var params = "?CAT=" + courseCAT + "&name=" + coursename + "&subject=" + coursesubject;
+    var params = "?CAT=" + courseCAT + "&name=" + coursename + "&subject=" + coursesubject + "&room" + "&semester" + "&year";
     var request = new XMLHttpRequest();
     request.open('POST', 'https://us-central1-management-system-be9f9.cloudfunctions.net/addCourse' + params, true)
     request.onload = function () {
@@ -450,6 +405,27 @@ function showAllProfessorData() {
     request.send()
 }
 
+// Makes request to API for data of a specific professor, calls generateTable()
+function showSpecificProfessorData() {
+
+    createTable("Professor Name","Professor ID","Email Add.");
+    var selectOpt =  document.getElementById("selectOption").value;
+    var searchInput = document.getElementById("mainSearchBar").value;
+    var params = "?key=" + selectOpt.toLowerCase() + "&value=" + searchInput;
+    console.log(searchInput,selectOpt);
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://us-central1-management-system-be9f9.cloudfunctions.net/getProfessors' +params, true)
+    request.onload = function() {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+        console.log(data[0].name);
+        star = data;
+        generateTable(data)
+    }
+
+    request.send()
+}
+
 // Makes request to API and removes a Professor
 function removeProfessorData() {
 
@@ -467,7 +443,7 @@ function removeProfessorData() {
 
 
 // Displays the pop up
-function addStudent() {
+function addStudent(){
     // Get the modal
     var modal = document.getElementById("myModal");
     // Get the button that opens the modal
@@ -477,11 +453,11 @@ function addStudent() {
     // When the user clicks the button, open the modal
     modal.style.display = "block";
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
+    span.onclick = function() {
         modal.style.display = "none";
     }
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
+    window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
@@ -489,7 +465,7 @@ function addStudent() {
 }
 
 // Displays the pop up
-function remStudent() {
+function remStudent(){
     // Get the modal
     var modal = document.getElementById("myModal2");
     // Get the button that opens the modal
